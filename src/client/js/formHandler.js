f9e831a5d37b8bef8d7c0ce56f125d36fc1bd2d9
Chris Darden <chris.a.darden@gmail.com>
2021-07-05 17:03:49 -0500

    removed debugging info

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,5 @@
 function checkURL(inputText) {
-    console.log("::: Running checkURL :::", inputText);
     const r = new RegExp(/^(http|https):\/\/[^ "]+$/);
     return r.test(inputText);
 }
-
 export { checkURL }
