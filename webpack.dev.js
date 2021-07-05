0da7e3822b527de784328634cf2d8f2514686256
Chris Darden <chris.a.darden@gmail.com>
2021-07-04 22:20:44 -0500

    commit 2

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+    // version 1
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
