0d6ebcba93218c28cb355a5ace442df820619535
Chris Darden <chris.a.darden@gmail.com>
2021-07-05 11:34:47 -0500

    added javascript/auto to rules module

--- src/client/index.js
+++ src/client/index.js
@@ -11,7 +11,6 @@ function handleSubmit(event) {
         // check what text was put into the form field
     let userURL = document.getElementById('userURL').value
     console.log(userURL);
-
     fetch('http://localhost:8080/test')
         .then(res => res.json())
     console.log('http://localhost:8080/test')
