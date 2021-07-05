68650db58d0fd9cafa6a9194252ca50c44c5f9ae
Chris Darden <chris.a.darden@gmail.com>
2021-07-05 16:58:59 -0500

    removed old function

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,3 @@
-function handleSubmit(event) {
-    event.preventDefault()
-        // check what text was put into the form field
-    let userURL = document.getElementById('userURL').value
-
-    fetch('http://localhost:8080/NLP')
-        .then(res => res.json())
-        .then(function(res) {
-            document.getElementById('results').innerHTML = res.message
-        })
-}
-
 function checkURL(inputText) {
     console.log("::: Running checkURL :::", inputText);
     const r = new RegExp(/^(http|https):\/\/[^ "]+$/);
@@ -17,4 +5,3 @@ function checkURL(inputText) {
 }
 
 export { checkURL }
-export { handleSubmit }
