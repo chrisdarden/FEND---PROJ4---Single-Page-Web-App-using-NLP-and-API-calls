98d42111380d34680c21d434c9a7b17de16ad37f
Chris Darden <chris.a.darden@gmail.com>
2021-07-05 16:41:18 -0500

    refactoring

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,12 +3,18 @@ function handleSubmit(event) {
         // check what text was put into the form field
     let userURL = document.getElementById('userURL').value
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/userText')
+    fetch('http://localhost:8080/NLP')
         .then(res => res.json())
         .then(function(res) {
             document.getElementById('results').innerHTML = res.message
         })
 }
 
+function checkURL(inputText) {
+    console.log("::: Running checkURL :::", inputText);
+    const r = new RegExp(/^(http|https):\/\/[^ "]+$/);
+    return r.test(inputText);
+}
+
+export { checkURL }
 export { handleSubmit }
